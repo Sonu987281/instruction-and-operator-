e303ed421ad1daed964de529fb56029d707d6ea6
Sonu987281 <saannukumarsingh@gmail.com>
2024-08-24 22:20:49 +0530

    Add files via upload

---
+++ 19_instruction.c
@@ -0,0 +1,69 @@
+//PRACTICE QUESTION
+
+
+
+#include<stdio.h>
+int main()
+{
+printf(" %d\n",6>7|| 5>6);
+return 0;
+}
+
+
+
+
+//Q.write a programn to print numbe r id divisivle by two 
+
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    printf(" enter an number :");
+    scanf(" %d",&num);
+
+    printf(" %d\n",num%2==0);
+    return 0;
+}
+
+
+
+ //Q.if sunday and its snow so print true 
+
+
+#include<stdio.h>
+int main()
+{
+    int issunday =0;
+    int issnow =1;
+    printf(" %d\n",issunday && issnow);
+    return 0;
+}
+
+
+//Q .wap to print  if monday and its rain so print true
+
+#include<stdio.h>
+int main()
+{
+    int ifmonday =0;
+    int ifraining =1;
+
+    printf(" %d\n",ifmonday || ifraining);
+    return 0;
+}
+
+// Q .wap to print if number is greater than 9 and less than 100 
+
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf(" enter an number :");
+    scanf(" %d",&num);
+
+    printf(" %d\n",num>=9 && num<=100);
+    return 0;
+}
+
